93e63c3dc5ca0314520bb858aadd87eff65143d8
JayceNgo <60013030+JayceNgo@users.noreply.github.com>
2024-03-12 15:19:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Group01_COMP304002_Lab03_Exercise01
+ 
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Group01_COMP304002_Lab03_Exercise01"
+include(":app")
+ 
